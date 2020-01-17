90122971efd925b66db5b488eba3cac55f1e7f94
Evans Ibok <evansibok@gmail.com>
2020-01-17 09:34:56 +0100

    create server

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/projects', (req, res) => {
+  res.status(200).json({ message: "Projects retrieved successfully!" })
+})
+
+app.use((req, res) => {
+  res.status(200).json({ message: "API is live!" });
+})
+
+module.exports = app;
